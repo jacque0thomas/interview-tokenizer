e33a43602875e2517732a65bb882bd5cf354ef66
Jacque <jacque.0.thomas@gmail.com>
2022-02-04 15:50:46 -0500

    first commit

---
+++ interview-tokenizer.py
@@ -0,0 +1,8 @@
+'''
+CAEN Round 3 
+Python Project
+Inputs: CL file location of text file with words that are whitespace delineated*
+Outputs: Summary information*; alphabetic dictionary file*
+*See original specs for more detail
+'''
+
