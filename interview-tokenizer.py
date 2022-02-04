f37c597600102bd2cb5b0c321c2412c2206259fe
Jacque <jacque.0.thomas@gmail.com>
2022-02-04 16:46:45 -0500

    Brainstorming documentation

--- interview-tokenizer.py
+++ interview-tokenizer.py
@@ -1,8 +1,14 @@
 '''
 CAEN Round 3 
 Python Project
-Inputs: CL file location of text file with words that are whitespace delineated*
-Outputs: Summary information*; alphabetic dictionary file*
+Inputs:     CL file location of text file with words that are whitespace delineated*
+Outputs:    Summary information*; alphabetic dictionary file*
 *See original specs for more detail
 '''
+
+
+# Main
+# INPUT:    Text file name on the command line
+# OUTPUT:   Summary to command line
+#           Alphabetized output of words and their length
 
