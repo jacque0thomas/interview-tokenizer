a73bdada61729b9946dce3daac301f49b378d126
Jacque <jacque.0.thomas@gmail.com>
2022-02-06 14:05:29 -0500

    working exercise, in process notes

--- coding-exercise.py
+++ coding-exercise.py
@@ -7,13 +7,14 @@ Outputs:    Summary information*; alphabetic dictionary*
 '''
 
 import os, argparse
-from string import strip
 
 # list of common non-alpha characters
 NONALPHA = "1234567890!@#$%^&*()-_=+\\\'\"[]\{\};:,./<>?`~"
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# COMMAND LINE FUNCTIONS
 
-
+# Retrieve command line arguments
 def parse_arguments():
     # Create the parser and add arguments
     parser = argparse.ArgumentParser()
@@ -29,45 +30,78 @@ def parse_arguments():
     
     return args.filename, args.v
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ASSERT FUNCTIONS
 
-# quits program if the file does not exist
+# Quits program if the file does not exist
 def assert_file_exists(filename):
     if not os.path.exists(filename): 
         print("File name \"" + filename + "\"does not exist from this directory.")
         print("Please ensure your filename is correct relative to the current working directory.")
         print("Your current working directory: " + os.getcwd())
         quit()
+
+def assert_words(word_count, filename):
+    if word_count == 0:
+        print("No words found in " + filename)
+        print("Please use a text file that contains words.")
+        quit()
         
-        
-# prints summary statistics according to specs
-def print_summary(word_count):
-    print("")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# OUTPUT FUNCTIONS
+
+# A sorted dictionary is useless to python
+# but can be useful to users. 
+def print_psuedo_len_dict(sorted_list):
+    print("{")
+    for item in sorted_list:
+        print("\t" + item + ": " + str(len(item)))
+    print("}")
+
+# Prints summary statistics according to specs
+def print_summary(word_count, uniq_word_count, third_frequent):
+    print("There are " + str(word_count) + " words in the file.")
+    print("There are " + str(uniq_word_count) + " unique words in the file.")
+    print("The third most frequent word in the file is \"" + third_frequent + ".\"")
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ASSEMBLES 
 
-# in: file 
+# reads the file, creates stats, outputs stats
 def assemble_file_stats(filename):
     
     # basic error checking 
     assert_file_exists(filename)
     
     word_count = 0
-    words = {}
+    word_freq = {}
+    
     # reads line and retrieves data
     with open(filename) as f:
         for line in f:
-            for word in line.split().strip(NONALPHA):
-                print(word)
+            for word in line.split():
+                word = word.strip(NONALPHA).lower()
                 
-    # checking if the input was garbage
-    if word_count == 0:
-        print("No words found in " + filename + ".")
-        print("Please use a text file that contains words.")
-                
-
-                
+                if len(word) != 0:
+                    word_count += 1
+                    
+                    if word in word_freq:
+                        word_freq[word] = word_freq[word] + 1
+                    else:
+                        word_freq[word] = 1
+                        
+    # asserting that the input was readable
+    assert_words(word_count, filename)
         
+    # sorting for frequency and alphabetically
+    sorted_words_by_freq = sorted(word_freq.items(), key=lambda x:x[1], reverse=True)
+    sorted_words_by_abc = sorted(word_freq.keys())
     
+    print_summary(word_count, len(word_freq), sorted_words_by_freq[3][0])
+    
+    print_psuedo_len_dict(sorted_words_by_abc)
+
 
 # main
 def main(): 
@@ -75,11 +109,9 @@ def main():
     # retrieves the command line arguments
     filename, verbose = parse_arguments()
     
-    # the main part of the program
+    # reads the file, creates stats, outputs stats
     assemble_file_stats(filename)
-    
-    print(filename)
-    print(verbose)
+
 
 # stub for main
 if __name__=="__main__":
